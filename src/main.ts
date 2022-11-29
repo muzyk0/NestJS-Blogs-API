245aef616ebfb36c5acbad3673c4b23eee58eed2
muzyk0 <ru9art@gmail.com>
2022-11-30 00:29:38 +0300

    vercel deploy
    (cherry picked from commit 70286b6a384ca5d192581c71c757a17c3ce425bd)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 import { ErrorExceptionFilter, HttpExceptionFilter } from './common/filters';
