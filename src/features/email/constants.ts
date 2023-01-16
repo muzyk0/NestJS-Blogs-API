d621fe71c6b91d73142c3e830b3540c12893b174
muzyk0 <ru9art@gmail.com>
2023-01-16 23:29:08 +0300

    revert rmq

--- src/features/email/constants.ts
+++ src/features/email/constants.ts
@@ -6,14 +6,18 @@ export const rabbitMQModule = ClientsModule.registerAsync([
     name: 'MESSAGE_SENDER_SERVICE',
     imports: [ConfigModule],
     useFactory: (configService: ConfigService) => {
+      console.warn(
+        'RMQ_URLS',
+        configService.get<string>('RMQ_URLS').split(', '),
+      );
       return {
-        transport: Transport.TCP,
+        transport: Transport.RMQ,
         options: {
-          // host: configService.get<string>('MESSAGE_SENDER_HOST'),
-          host: '137.184.177.20',
-          // host: '0.0.0.0',
-          // port: Number(configService.get<string>('MESSAGE_SENDER_PORT')),
-          // port: 443,
+          urls: configService.get<string>('RMQ_URLS').split(', '),
+          queue: 'message_sender_queue',
+          queueOptions: {
+            durable: false,
+          },
         },
       };
     },
