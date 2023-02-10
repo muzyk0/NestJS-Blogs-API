38142f17b238819ebdef9f73f8d4cf2cca193170
muzyk0 <ru9art@gmail.com>
2023-02-10 17:43:14 +0300

    fix folder name

--- src/features/super-admin/super-admin.module.ts
+++ src/features/super-admin/super-admin.module.ts
@@ -3,7 +3,7 @@ import { CqrsModule } from '@nestjs/cqrs';
 
 import { BlogsModule } from '../blogs/blogs.module';
 
-import { SuperAdminController } from './api /super-admin.controller';
+import { SuperAdminController } from './api/super-admin.controller';
 import { SuperAdminService } from './application/super-admin.service';
 import { BanBlogHandler } from './application/use-cases/ban-blog.handler';
 
