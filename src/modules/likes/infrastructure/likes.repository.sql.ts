eb62a74aee9cd196d12892914a021f2c5ff6635e
Музыка Владислав <ru9art@gmail.com>
2023-03-16 23:21:29 +0300

    fix create or update comment like status 3

--- src/modules/likes/infrastructure/likes.repository.sql.ts
+++ src/modules/likes/infrastructure/likes.repository.sql.ts
@@ -19,14 +19,28 @@ export class LikesRepositorySql {
   async createOrUpdatePostLikeStatus(
     createLike: Omit<LikeInterface, 'commentId'>,
   ): Promise<Like | null> {
-    const [like]: [Like] = await this.dataSource.query(
+    const [updatedLike]: [Like] = await this.dataSource.query(
       `
           UPDATE likes
           SET "status" = $3
           WHERE "userId" = $1
             AND "postId" = $2
           RETURNING *;
+          ;
+      `,
+      [
+        createLike.userId,
+        createLike.postId,
+        createLike.status ?? LikeStatus.NONE,
+      ],
+    );
 
+    if (updatedLike) {
+      return updatedLike;
+    }
+
+    const [createdLike]: [Like] = await this.dataSource.query(
+      `
           INSERT INTO likes ("userId", "postId", status)
           SELECT $1, $2, $3
 
@@ -43,27 +57,20 @@ export class LikesRepositorySql {
       ],
     );
 
-    return like;
+    return createdLike;
   }
 
   async createOrUpdateCommentLikeStatus(
     createLike: Omit<LikeInterface, 'postId'>,
   ): Promise<Like | null> {
-    const [like]: [Like] = await this.dataSource.query(
+    const [updatedLike]: [Like] = await this.dataSource.query(
       `
           UPDATE likes
           SET "status" = $3
           WHERE "userId" = $1
             AND "commentId" = $2
           RETURNING *;
-
-          INSERT INTO likes ("userId", "commentId", status)
-          SELECT $1, $2, $3
-
-          WHERE NOT EXISTS(SELECT 1
-                           FROM likes
-                           WHERE "userId" = $1
-                             AND "commentId" = $2)
+          ;
       `,
       [
         createLike.userId,
@@ -72,6 +79,28 @@ export class LikesRepositorySql {
       ],
     );
 
-    return like;
+    if (updatedLike) {
+      return updatedLike;
+    }
+
+    const [createdLike]: [Like] = await this.dataSource.query(
+      `
+          INSERT INTO likes ("userId", "commentId", status)
+          SELECT $1, $2, $3
+
+          WHERE NOT EXISTS(SELECT 1
+                           FROM likes
+                           WHERE "userId" = $1
+                             AND "commentId" = $2)
+          ;
+      `,
+      [
+        createLike.userId,
+        createLike.commentId,
+        createLike.status ?? LikeStatus.NONE,
+      ],
+    );
+
+    return createdLike;
   }
 }
